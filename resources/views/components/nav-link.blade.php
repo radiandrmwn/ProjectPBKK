5792ff73ebfc2195a9d0ea619d95cf8353ac64ce
radiandrmwn <radiantrydarmawan13@gmail.com>
2024-09-24 16:42:01 +0700

    Video 6

---
+++ resources/views/components/nav-link.blade.php
@@ -0,0 +1,3 @@
+@props(['active' => false])
+
+<a {{ $attributes }} class="{{ $active ? 'bg-gray-900 px-3 py-2 text-sm font-medium text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white'}}" aria-current="{{ $active ? 'page' : false }}" {{ $attributes }}>{{ $slot }}</a>
--- routes/web.php
+++ routes/web.php
@@ -3,17 +3,30 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', ['title' => 'Home Page']);
 });
 
 Route::get('/about', function () {
-    return view('about', ['nama'=> 'Sandhika Galih']);
+    return view('about', ['name' => 'Radian Try Darmawan' , 'title' => 'Contact']);
 });
 
-Route::get('/blog', function () {
-    return view('blog');
+Route::get('/posts', function () {
+    return view('posts', ['title' => 'Blog', 'posts' => [
+        [
+            'id' => 1,
+            'title' => 'Article Title 1',
+            'author' => 'Radian Try Darmawan',
+            'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dignissimos incidunt reiciendis cupiditate quidem quibusdam repellendus et, dicta molestias culpa ex reprehenderit iste minima doloribus quae adipisci veritatis unde voluptatem nostrum.'
+        ],
+        [
+            'id' => 2,
+            'title' => 'Article Title 2',
+            'author' => 'Radian Try Darmawan',
+            'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dignissimos incidunt reiciendis cupiditate quidem quibusdam repellendus et, dicta molestias culpa ex reprehenderit iste minima doloribus quae adipisci veritatis unde voluptatem nostrum.'
+        ]
+    ]]);
 });
 
 Route::get('/contact', function () {
-    return view('contact');
+    return view('contact', ['title' => 'Contact']);
 });
