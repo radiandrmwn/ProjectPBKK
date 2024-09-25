c7c4e1342a99ee2ac29546efb113fef9a1e7cc83
radiandrmwn <radiantrydarmawan13@gmail.com>
2024-09-25 11:21:53 +0700

    Video7-end

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -14,12 +15,14 @@ Route::get('/posts', function () {
     return view('posts', ['title' => 'Blog', 'posts' => [
         [
             'id' => 1,
+            'slug' => 'article-title-1',
             'title' => 'Article Title 1',
             'author' => 'Radian Try Darmawan',
             'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dignissimos incidunt reiciendis cupiditate quidem quibusdam repellendus et, dicta molestias culpa ex reprehenderit iste minima doloribus quae adipisci veritatis unde voluptatem nostrum.'
         ],
         [
             'id' => 2,
+            'slug' => 'article-title-2',
             'title' => 'Article Title 2',
             'author' => 'Radian Try Darmawan',
             'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dignissimos incidunt reiciendis cupiditate quidem quibusdam repellendus et, dicta molestias culpa ex reprehenderit iste minima doloribus quae adipisci veritatis unde voluptatem nostrum.'
@@ -28,21 +31,29 @@ Route::get('/posts', function () {
 });
 
 
-Route::get('/posts/{id}', function($id) {
+Route::get('/posts/{slug}', function($slug) {
     $posts = [
         [
             'id' => 1,
+            'slug' => 'article-title-1',
             'title' => 'Article Title 1',
             'author' => 'Radian Try Darmawan',
             'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dignissimos incidunt reiciendis cupiditate quidem quibusdam repellendus et, dicta molestias culpa ex reprehenderit iste minima doloribus quae adipisci veritatis unde voluptatem nostrum.'
         ],
         [
             'id' => 2,
+            'slug' => 'article-title-2',
             'title' => 'Article Title 2',
             'author' => 'Radian Try Darmawan',
             'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dignissimos incidunt reiciendis cupiditate quidem quibusdam repellendus et, dicta molestias culpa ex reprehenderit iste minima doloribus quae adipisci veritatis unde voluptatem nostrum.'
         ]
-        ];
+    ];
+
+        $post = Arr::first($posts, function ($post) use ($slug) {
+            return $post['slug'] == $slug;
+    });
+
+    return view('post', ['title' => 'Single Post','post' => $post]);
 });
 
 Route::get('/contact', function () {
