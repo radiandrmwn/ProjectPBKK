7c124dc36ae6ad31c35a2c48c99f77d67473e7d8
radiandrmwn <radiantrydarmawan13@gmail.com>
2024-10-02 13:39:55 +0700

    Providing 404 page for errors and returns

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -27,10 +27,14 @@ class Post
         ];
     }
 
-    public static function find($slug)
+    public static function find($slug): array
     {
-        return Arr::first(static::all(), function ($post) use ($slug) {
-            return $post['slug'] == $slug;
-        });
+        $post = Arr::first(static::all(), fn ($post) => $post['slug'] == $slug);
+
+        if (! $post) {
+            abort(404);
+        }
+
+        return $post;
     }
 }
