8dccb9ea1293bcf79afef19f4721080328d71f40
radiandrmwn <radiantrydarmawan13@gmail.com>
2024-10-11 23:51:12 +0700

    Small changes-downloaded flowbite

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -9,6 +9,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 class Post extends Model
 {
     use HasFactory;
+    
     protected $fillable = ['title', 'author', 'slug','body'];
 
     protected $with = ['author', 'category'];
