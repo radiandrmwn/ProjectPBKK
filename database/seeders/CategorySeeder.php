9485c3494af15e6de0040f5bb5e88473bda9f9d8
radiandrmwn <radiantrydarmawan13@gmail.com>
2024-10-12 19:49:22 +0700

    Finished video 15- Redesigning UI in both Posts and Single Post using templates from flowbite

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -16,22 +16,26 @@ class CategorySeeder extends Seeder
         // Category::factory(3)->create();
         Category::create([
             'name' => 'Web Design',
-            'slug' => 'web-Design'
+            'slug' => 'web-Design',
+            'color' => 'red'
         ]);
 
         Category::create([
             'name' => 'Machine Learning',
-            'slug' => 'machine-learning'
+            'slug' => 'machine-learning',
+            'color' => 'green'
         ]);
 
         Category::create([
             'name' => 'UI UX',
-            'slug' => 'ui-ux'
+            'slug' => 'ui-ux',
+            'color' => 'blue'
         ]);
 
         Category::create([
             'name' => 'Data Structure',
-            'slug' => 'data-structure'
+            'slug' => 'data-structure',
+            'color' => 'yellow'
         ]);
     }
 }
